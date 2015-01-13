b70c0f40e5120ce9f6f94612766ac42b721bbd29
Keith Lazuka <klazuka@acompli.com>
2015-01-12 21:50:11 -0800

    Fix incorrect local parameter name

--- FunctionalViewControllers/AppDelegate.swift
+++ FunctionalViewControllers/AppDelegate.swift
@@ -46,8 +46,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
         // Override point for customization after application launch.
-        window?.rootViewController = run(navigation, artists) { artist in
-            println("Selected \(artist.name)")
+        window?.rootViewController = run(navigation, artists) { album in
+            println("Selected \(album.name)")
         }
         return true
     }
